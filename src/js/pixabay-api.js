5903bf27888e5ef89df233a9ff22567c1f9686a6
Zakhar Veremchuk <zaharverem@gmail.com>
2025-06-18 13:27:42 +0300

    Change fetch on axios

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -1,14 +1,15 @@
+import axios from "axios";
+
 const token = "38994291-8fc9a2a1ed4020d28b7742733";
 
 export function fetchImages(query) {
-    const searchParams = new URLSearchParams({
-        key: token,
-        q: query,
-        image_type: "photo",
-        orientation: "horizontal",
-        safesearch: true
-    });
-    const url = `https://pixabay.com/api/?${searchParams}`;
-    const options = { method: "GET" };
-    return fetch(url, options).then(response => response.json());
+    return axios.get("https://pixabay.com/api/", {
+        params: {
+            key: token,
+            q: query,
+            image_type: "photo",
+            orientation: "horizontal",
+            safesearch: true
+        }
+    }).then(response => response.data);
 }
